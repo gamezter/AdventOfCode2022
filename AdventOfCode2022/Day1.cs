ba0bc71e84abd7a7008f295595c76a0e0062321a
Alexandre Charron <charronalexandre@hotmail.com>
2022-12-01 20:43:28 -0500

    initial commit

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2022
+{
+    class Day1
+    {
+        public static void part1()
+        {
+            string[] numbers = File.ReadAllLines("day1.txt");
+            int[] n = new int[numbers.Length];
+            for(int i = 0; i < n.Length; i++)
+            {
+                n[i] = int.Parse(numbers[i]);
+            }
+
+            int increases = 0;
+
+            for(int i = 0; i < n.Length - 1; ++i)
+            {
+                if (n[i] < n[i + 1])
+                    increases++;
+            }
+            
+            Console.WriteLine(increases);
+            Console.Read();
+        }
+
+        public static void part2()
+        {
+            string[] numbers = File.ReadAllLines("day1.txt");
+            int[] n = new int[numbers.Length];
+            for (int i = 0; i < n.Length; i++)
+            {
+                n[i] = int.Parse(numbers[i]);
+            }
+
+            int increases = 0;
+
+            for (int i = 0; i < n.Length - 3; ++i)
+            {
+                if (n[i] < n[i + 3])
+                    increases++;
+            }
+
+            Console.WriteLine(increases);
+            Console.Read();
+        }
+    }
+}
