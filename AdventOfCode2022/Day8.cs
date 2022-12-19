947cb4f2a2b7c7c6b8c37d7774502e85fd45c904
Alexandre Charron <charronalexandre@hotmail.com>
2022-12-18 20:53:34 -0500

    solve day 8 part 2

--- AdventOfCode2022/Day8.cs
+++ AdventOfCode2022/Day8.cs
@@ -84,5 +84,81 @@ namespace AdventOfCode2022
             Console.WriteLine(count);
             Console.Read();
         }
+
+        public static void part2()
+        {
+            string[] lines = File.ReadAllLines("day8.txt");
+
+            int width = lines[0].Length;
+            int height = lines.Length;
+
+            int[,] score = new int[width, height];
+
+            for (int y = 0; y < height; ++y) // left side
+            {
+                int[] highest = new int[10];
+                for (int x = 0; x < width; ++x)
+                {
+                    int tree = lines[y][x] - '0';
+                    score[x, y] = x - highest[tree];
+                    for (int i = 0; i <= tree; ++i)
+                        highest[i] = x;
+                }
+            }
+
+            for (int y = 0; y < height; ++y) // right side
+            {
+                int[] highest = new int[10];
+                for (int i = 0; i < 10; ++i)
+                    highest[i] = width - 1;
+                for (int x = width - 1; x >= 0; --x)
+                {
+                    int tree = lines[y][x] - '0';
+                    score[x, y] *= highest[tree] - x;
+                    for (int i = 0; i <= tree; ++i)
+                        highest[i] = x;
+                }
+            }
+
+            for (int x = 0; x < width; ++x) // top side
+            {
+                int[] highest = new int[10];
+                for (int y = 0; y < height; ++y)
+                {
+                    int tree = lines[y][x] - '0';
+                    score[x, y] *= y - highest[tree];
+                    for (int i = 0; i <= tree; ++i)
+                        highest[i] = y;
+                }
+            }
+
+            for (int x = 0; x < width; ++x) // bottom side
+            {
+                int[] highest = new int[10];
+                for (int i = 0; i < 10; ++i)
+                    highest[i] = height - 1;
+                for (int y = height - 1; y >= 0; --y)
+                {
+                    int tree = lines[y][x] - '0';
+                    score[x, y] *= highest[tree] - y;
+                    for (int i = 0; i <= tree; ++i)
+                        highest[i] = y;
+                }
+            }
+
+            int max = 0;
+
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    if (score[x, y] > max)
+                        max = score[x, y];
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.Read();
+        }
     }
 }
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2022
     {
         static void Main(string[] args)
         {
-            Day8.part1();
+            Day8.part2();
         }
     }
 }
